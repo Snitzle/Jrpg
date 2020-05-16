cd17a66252c602b6b7933b61d433a856d97bea16
Craig Jones <iamcraig25@hotmail.co.uk>
2020-05-16 20:04:30 +0100

    reworked map to use hashmaps. need to make an API to make the rendering easier now

--- src/Map.java
+++ src/Map.java
@@ -1,20 +1,39 @@
-public class Map {
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
 
-    private static int[][] Map;
+public class Map implements MapInterface {
 
-    public static int[][] getMap() {
+    private List<List<HashMap<String, Integer>>> Map;
+
+    public List<List<HashMap<String, Integer>>> getMap() {
         if (Map == null) {
-            Map = new int[Game.gridWidth][Game.gridHeight];
+            Map = new ArrayList<>();
+            for (int i = 0; i < Game.gridWidth; i++) {
+
+                ArrayList<HashMap<String, Integer>> column = new ArrayList<>();
+
+                for (int j = 0; j < Game.gridHeight; j++) {
+
+                    HashMap<String, Integer> cell = new HashMap<>();
+                    cell.put("key", 0);
+                    cell.put("enemyID", 0);
+
+                    column.set(j, cell);
+
+                }
+                Map.set(i, column);
+            }
         }
         return Map;
     }
 
-    public static void setMapPosition(int x, int y, int key) {
-        Map[x][y] = key;
+    public void setMapPosition(int x, int y, int key) {
+        Map.get(x).get(y).put("key", key);
     }
 
-    public static int getMapKey(int x, int y) {
-        return Map[x][y];
+    public int getMapKey(int x, int y) {
+        return Map.get(x).get(y).get("key");
     }
 
 }
---
+++ src/MapInterface.java
@@ -0,0 +1,7 @@
+public interface MapInterface {
+
+    void setMapPosition(int x, int y, int key);
+
+    int getMapKey(int x, int y);
+
+}
