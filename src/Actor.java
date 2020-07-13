bf720c327829d3dff56ec8e184f3117537a8779a
Craig Jones <iamcraig25@hotmail.co.uk>
2020-07-13 21:35:43 +0100

    Refactored Actor to use Position

--- src/Actor.java
+++ src/Actor.java
@@ -3,49 +3,29 @@ public abstract class Actor {
     /**
      * This is the base object that contains generic data that every actor in the game requires.
      */
-    protected int x, y;
-    protected ID id;
+    private Position pos;
     protected int health;
 
     protected int moveStep = 32;
 
-    public Actor(int x, int y, ID id) {
-        this.x = x;
-        this.y = y;
-        this.id = id;
+    public Actor(int x, int y, int key) {
+
+        this.pos.setX(x);
+        this.pos.setY(y);
+        this.pos.setKey(key);
+
     }
 
-    public Actor(int x, int y, ID id, int health) {
-        this.x = x;
-        this.y = y;
-        this.id = id;
+    public Actor(int x, int y, int key, int health) {
+
+        this.pos.setX(x);
+        this.pos.setY(y);
+        this.pos.setKey(key);
+
         this.health = health;
+        
     }
 
     public abstract void tick();
-//    public abstract void render(Graphics g);
 
-    public float getX() {
-        return x;
-    }
-
-    public void setX(int x) {
-        this.x = x;
-    }
-
-    public float getY() {
-        return y;
-    }
-
-    public void setY(int y) {
-        this.y = y;
-    }
-
-    public ID getId() {
-        return id;
-    }
-
-    public void setId(ID id) {
-        this.id = id;
-    }
 }
